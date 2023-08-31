88f1cddcc4328f88663d28e46174b7f5dd43c3ae
Gabriel Sola <gabrielmartinsola99988@gmail.com>
2023-08-31 17:13:01 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/InfraestructuraControllercs.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebNotas.Controllers
+{
+    public class InfraestructuraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Folleto");
+        }
+
+        public IActionResult Folleto()
+        {
+            return View();
+        }
+
+        public IActionResult Primero()
+        {
+            ViewData["Año"] = "Primer Año";
+            return View("Materias");
+        }
+
+        public IActionResult Segundo()
+        {
+            ViewData["Año"] = "Segundo Año";
+            return View("Materias");
+        }
+
+        public IActionResult Tercero()
+        {
+            ViewData["Año"] = "Tercer Año";
+            return View("Materias");
+        }
+    }
+
+
+}
---
+++ Models/WebNotasContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebNotas.Models
+{
+    public class WebNotasContext : DbContext
+    {
+        public WebNotasContext(DbContextOptions<WebNotasContext>options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Nota> Notas { get; set; }
+    }
+}
